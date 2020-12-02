7149f0aada5f3046ca901442cf5a0a03de8e42d6
00009815 <72667153+00009815@users.noreply.github.com>
2020-12-02 18:08:45 +0500

    task 1-homework

---
+++ main.py
@@ -0,0 +1,28 @@
+def countdown(n):
+    if n <= 0:
+        print('Blastoff!')
+    else:
+        print(n)
+        countdown(n - 1)
+
+
+def countup(n):
+    if n >= 0:
+        print('Blastoff!')
+    else:
+        print(n)
+        countup(n + 1)
+
+
+number = int(input("Type the number: "))
+
+def my_function(n):
+    if n > 0:
+        countdown(n)
+    elif n < 0:
+        countup(n)
+    else:
+        print('Blastoff!')
+
+
+my_function(number)
